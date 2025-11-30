fceb0f62fe4329ee92924c83a945145de02b812c
A. Edson Maribone <allan.edson@maribone.com.br>
2025-11-30 20:34:51 -0300

    Update complemento.js

--- complemento.js
+++ complemento.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
         // Configuração do mapa
-        var map = L.map('map').setView([-23.5505, -46.6333], 2); // Ajuste a localização inicial e o nível de zoom
+        var map = L.map('map').setView([-23.4795, -46.3685], 14); // Ajuste a localização inicial e o nível de zoom
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19
