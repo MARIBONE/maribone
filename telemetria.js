18897756f43ccf21d61054cd5126e383f10acdbc
A. Edson Maribone <allan.edson@maribone.com.br>
2026-09-02 22:41:57 -0300

    Update telemetria.js

--- telemetria.js
+++ telemetria.js
@@ -8,7 +8,7 @@
     // ============================================================
 
     const CONFIG = {
-        API_URL: "https://api.maribone.com.br/telemetria",
+        API_URL: "https://api.maribone.com.br/telemetria", 
         API_VERSAO: "1.0.0",
 
         CHAVE_SESSAO: "maribone_id_sessao",
